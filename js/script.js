a6a7c1b0f14b62c8b4675c77e7fb1381d8c27fa0
juan david marin <juaanmarin2007@gmail.com>
2022-04-22 22:37:43 -0500

    agregue la logica para aobtener las notas y cambiar el color de fondo

--- js/script.js
+++ js/script.js
@@ -4,23 +4,12 @@ const note2=document.getElementById("note2");
 const color=document.getElementById("background");
 const validate=document.getElementById("button");
 const changebg=document.getElementById("changeBG");
+const btnRecoveryNote=document.getElementById("btnRecoveryNote");
+const btnMissingNote=document.getElementById("btnmissingNote");
+const btnNoteToPassTheSubject=document.getElementById("btnnoteToPassTheSubject");
+const btnRollOfHonor=document.getElementById("btnrollOfHonor");
 const result=document.getElementById("result");
 
-const cambiarfondo=()=>{
-    console.error("cambiar color de fondo")
-}
-
-const calculateNotes=(values)=>{
-    let noteFinal=(values.note1+values.note2)/2;
-
-    result.innerText=`por el momento la nota actual es ${noteFinal}`
-}
-
-const convertValues=()=>{
-    let convertNote1=parseFloat(note1.value);
-    let convertNote2=parseFloat(note2.value);
-    return values={note1:convertNote1, note2:convertNote2};
-}
 
 const validateFields=()=>{
     if(note1.value=="" || note2.value==""){
@@ -41,8 +30,60 @@ const validateFields=()=>{
     }
 }
 
+const convertValues=()=>{
+    let convertNote1=parseFloat(note1.value);
+    let convertNote2=parseFloat(note2.value);
+    return values={note1:convertNote1, note2:convertNote2};
+}
+
+const calculateNotes=()=>{
+    let values=convertValues()
+    let cal=((values.note1*0.3) + (values.note2*0.3));
+    return cal;
+}
+
+const NoteForrollOfHonor=()=>{
+    let number=calculateNotes();
+    let noteFinal=(4.0-number)/0.4;
+    result.innerText=`la nota minima para apracer en el cuadro de honor es de ${noteFinal} `;
+}
+
+const missingNote=()=>{
+    let number=calculateNotes();
+    let noteFinal=(1-number)/0.4;
+    result.innerText=`la nota que debe sacar para perder la materia es ${noteFinal} `;
+}
+
+const approvalNote=() =>{
+    let number=calculateNotes();
+    let noteFinal=(3.5-number)/0.4;
+    result.innerText=`la nota minima para aprobar es de ${noteFinal} `;
+}
+
+const recoveryNote=() =>{
+    let number=calculateNotes();
+    let noteFinal=(2-number)/0.4;
+    result.innerText=`la nota minima para recuperar es de ${noteFinal} `; 
+}
+
 changebg.addEventListener("click",() =>{
-    cambiarfondo();
+    let actual=document.body;
+    actual.style.background=color.value
+})
+
+btnRollOfHonor.addEventListener("click",() =>{
+    NoteForrollOfHonor();
+})
+btnMissingNote.addEventListener("click",() =>{
+    missingNote();
+})
+
+btnNoteToPassTheSubject.addEventListener("click",() =>{
+    approvalNote();
+})
+
+btnRecoveryNote.addEventListener("click",() =>{
+    recoveryNote();
 })
 
 validate.addEventListener("click", () =>{  
